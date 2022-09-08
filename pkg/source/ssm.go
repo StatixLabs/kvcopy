20aea226f11bfaba4294086381620ad37e351c76
Jake <jake@statixlabs.com>
2022-09-08 15:39:46 -0400

    Add SSM Path output to ENV (#1)
    * export ssm path to .env
    
    * add parameter flags for cli

--- pkg/source/ssm.go
+++ pkg/source/ssm.go
@@ -1,17 +1,26 @@
 package source
 
 import (
+	"strings"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ssm"
 )
 
 // createClient creates a Simple Systems Manager client
-func SSM(sess *session.Session, region string, name string) (string, error) {
+func SSM(sess *session.Session, region string, name string) (map[string]string, error) {
 	ssmsvc := ssm.New(sess, aws.NewConfig().WithRegion(region))
-	param, err := ssmsvc.GetParameter(&ssm.GetParameterInput{
-		Name:           aws.String(name),
-		WithDecryption: aws.Bool(false),
+	output := make(map[string]string)
+	param, err := ssmsvc.GetParametersByPath(&ssm.GetParametersByPathInput{
+		Path:           aws.String(name),
+		WithDecryption: aws.Bool(true),
 	})
-	return *param.Parameter.Value, err
+	if err != nil {
+		return output, err
+	}
+	for _, parameter := range param.Parameters {
+		output[strings.ReplaceAll(*parameter.Name, name, "")] = *parameter.Value
+	}
+	return output, nil
 }
