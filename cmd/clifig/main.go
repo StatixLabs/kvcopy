629222e9fbffd110ffd49be63e7f475482264ec1
Jake Alves <jake@statixlabs.com>
2022-09-08 15:36:00 -0400

    add parameter flags for cli

--- cmd/clifig/main.go
+++ cmd/clifig/main.go
@@ -3,21 +3,34 @@ package main
 import (
 	"clifig/pkg/auth"
 	"clifig/pkg/source"
+	"clifig/pkg/target"
+	"flag"
 	"fmt"
-	"os"
 )
 
 func main() {
-	region := os.Args[1]
-	profile := os.Args[2]
-	key := os.Args[3]
-	sess, err := auth.AWS(region, profile)
+	region := flag.String("region", "us-east-1", "Set the AWS region.")
+	profile := flag.String("profile", "default", "Set the AWS SSO profile to use.")
+	keyPointer := flag.String("ssm-parameter-path", "", "set the SSM parameter store key path. It must being and end with a '/'")
+	flag.Parse()
+	key := string(*keyPointer)
+	if key[:1] != "/" {
+		panic("'ssm-parameter-path' must begin with an '/'")
+	}
+	if key[len(key)-1:] != "/" {
+		panic("'ssm-parameter-path' must end with an '/'")
+	}
+
+	sess, err := auth.AWS(*region, *profile)
 	if err != nil {
 		fmt.Println(err)
 	}
-	param, err := source.SSM(sess, region, key)
+
+	param, err := source.SSM(sess, *region, key)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
-	fmt.Println(param)
+
+	output := target.Env(param)
+	fmt.Println(output)
 }
